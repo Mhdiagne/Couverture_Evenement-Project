0feb9b0a015010167d20f1c0fbd8446171f062d4
CMCode2001 <cheikh.mb2022@gmail.com>
2024-04-09 19:37:28 +0000

    Amelioration du menu d'accueil et creation de la navigation sur les differentes pages

---
+++ couverture_evenement-frontend/src/Pages/AjouterDemande.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+const AjouterDemande = () => {
+        return (
+            <div>
+            <Header/>
+            <div className="contenaire">
+                <h2> Ajouter Demande </h2>
+            </div>
+            </div>
+        );
+}
+
+
+export default AjouterDemande;
---
+++ couverture_evenement-frontend/src/Pages/VoirMesDemandes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+const VoirMesDemandes = () => {
+        return (
+            <div>
+            <Header/>
+            <div className="contenaire">
+                <h2>Voir mes demandes </h2>
+            </div>
+            </div>
+        );
+}
+
+
+export default VoirMesDemandes;
