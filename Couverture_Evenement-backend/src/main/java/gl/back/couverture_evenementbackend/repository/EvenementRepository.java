d5f5fda1bbfe26389e3fd722cb977d5d7258bd9d
CMCode2001 <cheikh.mb2022@gmail.com>
2024-04-13 01:49:50 +0000

    Ajout du Dashbord Admin et integration du PDFGenerator

--- Couverture_Evenement-backend/src/main/java/gl/back/couverture_evenementbackend/repository/EvenementRepository.java
+++ Couverture_Evenement-backend/src/main/java/gl/back/couverture_evenementbackend/repository/EvenementRepository.java
@@ -2,8 +2,14 @@ package gl.back.couverture_evenementbackend.repository;
 
 import gl.back.couverture_evenementbackend.entity.Evenement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 public interface EvenementRepository extends JpaRepository<Evenement, Long> {
+    @Query("SELECT e FROM Evenement e WHERE e.archive = true")
+    List<Evenement> findByArchiveTrue();
+
 }
